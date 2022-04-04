908b5883a93f32f49b451cbb2f3a044ce20e0d7d
TomCat65 <talvarez.uh@gmail.com>
2022-04-04 18:27:43 -0500

    SWR hook with fallback initial data prefetched better typed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import type { NextPage } from 'next'
 import GetPosts from '../components/getPosts'
 import styles from '../styles/Home.module.css'
 import { useFetch } from '../utilities'
-import { SWRConfig } from 'swr'
+import { SWRConfig, SWRConfiguration } from 'swr'
+import { Fallback } from '../typings'
 
 
 
 
 
-const Home: NextPage = ({fallback}) => {
+const Home: NextPage = ({fallback}:any) => {
   return (
     <div className={styles.container}>
      
       <h1>SWR hook use</h1>
       <SWRConfig value={{fallback}}>
-        {console.log('fallback',fallback)}
+        {console.log('fallback:',fallback)}
          <GetPosts />
       </SWRConfig>
      
